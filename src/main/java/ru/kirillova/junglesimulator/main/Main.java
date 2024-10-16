5f48e21351528c9afbe891196b995d97976d2bf6
Nataliya <ntliekirillova@gmail.com>
2024-10-16 22:45:03 +0300

    created method main

--- src/main/java/ru/kirillova/junglesimulator/main/Main.java
+++ src/main/java/ru/kirillova/junglesimulator/main/Main.java
@@ -1,4 +1,13 @@
 package ru.kirillova.junglesimulator.main;
 
+import ru.kirillova.junglesimulator.entity.Leopard;
+import ru.kirillova.junglesimulator.util.EventSimulator;
+
 public class Main {
+
+    public static void main(String[] args) {
+    Leopard leopard = new Leopard();
+    EventSimulator eventSimulator = new EventSimulator();
+    eventSimulator.startSimulation(leopard);
+    }
 }
