04c4d7b15f2b155d425da495f2c36be3f6ffa5d6
Nataliya <ntliekirillova@gmail.com>
2024-10-16 22:45:21 +0300

    created method startSimulation

--- src/main/java/ru/kirillova/junglesimulator/util/EventSimulator.java
+++ src/main/java/ru/kirillova/junglesimulator/util/EventSimulator.java
@@ -4,22 +4,45 @@ import ru.kirillova.junglesimulator.entity.Leopard;
 
 public class EventSimulator {
 
-    //леопард поспал - +20 энергии
-    //леопард побегал - 10 энергии
-    //на леопарда напали гиены - 35 здоровья
-    //на леопарда напал браконьер - 25 здоровья
-    //леопард переплывает реку - 15 энергии
-    //леопард съел антилопу - 8 энергия; коэфф * 8 здоровья
-    //леопард съел птицу  + 3 здоровья
-    //леопард съел зебру - 5 энергия; коэфф * 8 здоровья
-    //леопард отдыхает + 6 энергия
-    //леопард охотится - 10 энергия
+  //0 -10  //леопард поспал - +20 энергии
+   // 10 - 15 // леопард побегал - 10 энергии
+   // 15- 20 //на леопарда напали гиены - 35 здоровья
+   // 20 - 30 //на леопарда напал браконьер - 25 здоровья
+    // 30 - 35//леопард переплывает реку - 15 энергии
+   //35 - 40 //леопард съел антилопу - 8 энергия; коэфф * 8 здоровья
+    // 40 - 50//леопард съел птицу  + 3 здоровья
+   // 50 = 60 //леопард съел зебру - 5 энергия; коэфф * 8 здоровья
+    // 60 - 80 //леопард отдыхает + 6 энергия
+   //80 - 100 //леопард охотится - 10 энергия
     //энергия = 0 то -7 здоровья
 
-    public void activateEvent() {
-
-    }
-
+    public void startSimulation(Leopard leopard) {
+        while(checkStatus(leopard)) {
+            int eventNumber = (int) (Math.random() * 100);
+            if (eventNumber <= 0 && eventNumber < 10) {
+                sleepEvent(leopard);
+            } else if (eventNumber >= 10 && eventNumber < 15) {
+                move(leopard);
+            } else if (eventNumber >= 15 && eventNumber < 20) {
+                hyenasAttack(leopard);
+            } else if (eventNumber >= 20 && eventNumber < 30) {
+                poacherAttack(leopard);
+            } else if (eventNumber >= 30 && eventNumber < 35) {
+                swim(leopard);
+            } else if (eventNumber >= 35 && eventNumber < 40) {
+                leopardEatAntelope(leopard);
+            }  else if (eventNumber >= 40 && eventNumber < 50) {
+                leopardEatBird(leopard);
+            } else if (eventNumber >= 50 && eventNumber < 60) {
+                leopardEatZebra(leopard);
+            } else if (eventNumber >= 60 && eventNumber < 80) {
+                restEvent(leopard);
+            } else if (eventNumber >= 80 && eventNumber < 100) {
+                huntEvent(leopard);
+            }
+        }
+        System.out.println("Леопард умер! The end!");
+}
     private void sleepEvent(Leopard leopard) {
         int energy = leopard.getEnergy();
         energy = energy + 20;
@@ -27,6 +50,7 @@ public class EventSimulator {
             energy = 100;
         }
         leopard.setEnergy(energy);
+        checkEnergy(leopard);
         System.out.println("Леопард поспал! + 20 энергии. Текущая энергия: " + leopard.getEnergy());
     }
 
@@ -37,6 +61,7 @@ public class EventSimulator {
             energy = 0;
         }
         leopard.setEnergy(energy);
+        checkEnergy(leopard);
         System.out.println("Леопард побегал! -10 энергии. Текущая энергия: " + leopard.getEnergy());
     }
 
@@ -47,6 +72,7 @@ public class EventSimulator {
             health = 0;
         }
         leopard.setHealth(health);
+        checkEnergy(leopard);
         System.out.println("На леопарда напали гиены! - 35 здоровья. Текущее здоровье: " + leopard.getHealth());
     }
 
@@ -57,6 +83,7 @@ public class EventSimulator {
             health = 0;
         }
         leopard.setHealth(health);
+        checkEnergy(leopard);
         System.out.println("На леопарда напал браконьер! - 25 здоровья. Текущее здоровье: " + leopard.getHealth());
     }
 
@@ -67,6 +94,7 @@ public class EventSimulator {
             energy = 0;
         }
         leopard.setEnergy(energy);
+        checkEnergy(leopard);
         System.out.println("Леопард переплывает реку! - 15 энергии. Текущая энергия: " + leopard.getEnergy());
     }
 
@@ -83,6 +111,7 @@ public class EventSimulator {
         }
         leopard.setEnergy(energy);
         leopard.setHealth(health);
+        checkEnergy(leopard);
         System.out.println("Леопард съел антилопу! Текущая энергия: " + leopard.getEnergy() + " Текущее здоровье: " + leopard.getHealth());
     }
 
@@ -93,6 +122,7 @@ public class EventSimulator {
             health = 100;
         }
         leopard.setHealth(health);
+        checkEnergy(leopard);
         System.out.println("Леопард съел птицу! Текущее здоровье: " + leopard.getHealth());
     }
 
@@ -109,6 +139,7 @@ public class EventSimulator {
         }
         leopard.setEnergy(energy);
         leopard.setHealth(health);
+        checkEnergy(leopard);
         System.out.println("Леопард съел зебру! Текущая энергия: " + leopard.getEnergy() + " Текущее здоровье: " + leopard.getHealth());
     }
 
@@ -119,19 +150,22 @@ public class EventSimulator {
             energy = 100;
         }
         leopard.setEnergy(energy);
+        checkEnergy(leopard);
         System.out.println("Леопард отдохнул! + 6 энергии. Текущая энергия: " + leopard.getEnergy());
     }
 
     private void huntEvent(Leopard leopard) {
         int energy = leopard.getEnergy();
+
         energy = energy - 10;
         if (energy < 0) {
             energy = 0;
         }
         leopard.setEnergy(energy);
+        checkEnergy(leopard);
         System.out.println("Леопард охотится! - 10 энергии. Текущая энергия: " + leopard.getEnergy());
     }
-    private boolean statusChecker(Leopard leopard) {
+    private boolean checkStatus(Leopard leopard) {
         if (leopard.getHealth() <= 0) {
             return false;
         } else {
@@ -139,7 +173,7 @@ public class EventSimulator {
         }
     }
 
-    private void energyChecker(Leopard leopard) {
+    private void checkEnergy(Leopard leopard) {
         if(leopard.getEnergy() <= 0) {
             int health = leopard.getHealth();
             health = health - 7;
