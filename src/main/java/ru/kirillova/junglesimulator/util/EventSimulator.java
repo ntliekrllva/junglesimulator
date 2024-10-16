2839462bb42aada3693c34cef7cdd7a463f7f49d
Nataliya <ntliekirillova@gmail.com>
2024-10-16 20:03:24 +0300

    created method hyenasAttack

--- src/main/java/ru/kirillova/junglesimulator/util/EventSimulator.java
+++ src/main/java/ru/kirillova/junglesimulator/util/EventSimulator.java
@@ -5,7 +5,7 @@ import ru.kirillova.junglesimulator.entity.Leopard;
 public class EventSimulator {
 
     //леопард поспал - +20 энергии
-    //леопард побегал - 7 энергии
+    //леопард побегал - 10 энергии
     //на леопарда напали гиены - 35 здоровья
     //на леопарда напал браконьер - 25 здоровья
     //энергия = 0 то -7 здоровья
@@ -28,5 +28,25 @@ public class EventSimulator {
         leopard.setEnergy(energy);
         System.out.println("Леопард поспал! + 20 энергии. Текущая энергия: " + leopard.getEnergy());
     }
+
+    private void move(Leopard leopard){
+        int energy = leopard.getEnergy();
+        energy = energy - 10;
+        if(energy < 0) {
+            energy = 0;
+        }
+        leopard.setEnergy(energy);
+        System.out.println("Леопард побегал! -10 энергии. Текущая энергия:" + leopard.getEnergy());
+    }
+    private void hyenasAttack(Leopard leopard) {
+        int health = leopard.getHealth();
+        health = health - 35;
+        if(health < 0) {
+            health = 0;
+        }
+        leopard.setHealth(health);
+    }
+
+
 
 }
